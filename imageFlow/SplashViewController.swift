5055641bd66a20adde9524472ce32f9d25a2953f
Alexander <vinsanity305@mail.ru>
2023-10-04 11:10:48 +0300

    DONE: deleted "token" in method fetchProfile.

--- imageFlow/SplashViewController.swift
+++ imageFlow/SplashViewController.swift
@@ -96,7 +96,7 @@ extension SplashViewController: AuthViewControllerDelegate {
     }
     
     private func fetchProfile(_ token: String) {
-        profileService.fetchProfile(token+"token") { [weak self] result in
+        profileService.fetchProfile(token) { [weak self] result in
             DispatchQueue.main.async  {
                 guard let self = self else { return }
                 switch result {
